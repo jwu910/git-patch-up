034982e7b5210a7512224e92c5eabcf981fe483f
Joshua Wu <Joshua.wu.0910@gmail.com>
2018-02-28 16:40:10 -0800

    Initial structure for creating patch files. Still need to check for existing files. and add in rest of the patch utilities

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
+#!/usr/bin/env node
+
 const program = require('commander');
 const chalk = require('chalk');
+const git = require('./utils/git');
 
 program.version('0.0.1', '-v, --version');
 
 program
-	.arguments('<count> <gitSha>')
-	.action((count, gitSha) => {
-		numOfCommits = count;
-		startSha = gitSha;
+  .option('-n, --number <number>', 'Number of commits')
+  .option('-c, --commit <commit_sha>', 'Git commit sha')
+  .parse(process.argv);
 
-		// Establish dir to store patch files
-		// Check existing patch files
-		// Take arguments and search for git commit matching sha
-			// Resolve error if none found
-			// If sha found, # of commits past starting sha
-			
-	})
+const { commit, number } = program;
+const patchDir = './patch_files';
 
-program.parse(process.argv);
+git.createPatchFiles(number, commit, patchDir).then(() => {
+  console.log(chalk.green('Created patch files in ' + patchDir));
+});
+
+program.on('--help', function() {
+  console.log('  Examples:');
+  console.log('');
+  console.log('    $ gtp -n 5 -c 4c2615da4c02dd2602b969e5457bfa5e77085238');
+  console.log(
+    '    $ // Will create 5 patch files for the 5 most recent commits starting with the provided git SHA'
+  );
+  console.log('');
+});
+
+if (!process.argv.slice(2).length) {
+  program.help();
+}
---
+++ utils/git.js
@@ -0,0 +1,70 @@
+const { spawn } = require('child_process');
+
+function execGit(args) {
+  /*
+  Execute git command with passed arguments.
+  <args> is expected to be an array of strings.
+  Example: ['fetch', '-pv']
+  */
+  return new Promise((resolve, reject) => {
+    const gitResponse = spawn('git', args, {
+      cwd: process.cwd(),
+      silent: true
+    });
+
+    var retVal = '';
+
+    gitResponse.stdout.on('data', data => {
+      retVal += data.toString();
+    });
+
+    gitResponse.stdout.on('close', () => {
+      resolve(retVal.trim());
+    });
+
+    gitResponse.stderr.on('data', stderr => {
+      reject(stderr.toString());
+    });
+  });
+}
+
+function checkDir(patchDir) {
+  /*
+  Check if patch directory is created. Create if not yet created
+  */
+  return new Promise((resolve, reject) => {
+    const checkDir = spawn('mkdir', ['-p', patchDir], {
+      cwd: process.cwd(),
+      silent: false
+    });
+
+    var retVal = '';
+
+    checkDir.stdout.on('data', data => {
+      retVal += data.toString();
+    });
+
+    checkDir.stdout.on('close', () => {
+      resolve(retVal.trim());
+    });
+
+    checkDir.stderr.on('data', stderr => {
+      reject(stderr.toString());
+    });
+  });
+}
+
+async function createPatchFiles(number, commit, patchDir) {
+  /*
+  Execute checkDir to ensure directory is created. Call execGit to create patch files.
+  */
+  await checkDir(patchDir);
+
+  const args = ['format-patch', '-' + number, commit, '-o', patchDir, '-q'];
+
+  await execGit(args);
+}
+
+module.exports = {
+  createPatchFiles: createPatchFiles
+};
